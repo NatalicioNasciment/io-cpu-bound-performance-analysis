a0fa64f719d845ec393ddbe69db328c9868536e6
NatalicioNascimento22 <natalicio.eng.soft@gmail.com>
2024-05-08 22:15:33 -0300

    removendo palavra reservada async do endpoint sincrono

--- routers.py
+++ routers.py
@@ -4,7 +4,7 @@ from cats_api import download_cat_image, async_download_cat_image, status_list
 router = APIRouter()
 
 @router.get('/sync/images/download')
-async def sync_download_all_images():
+def sync_download_all_images():
     quantity = sum([download_cat_image(status) for status in status_list])
 
     return {'message': f'{quantity} images downloaded successfully.'}
